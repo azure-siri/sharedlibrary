ae72a4b70ea7738301596c1835609c5f7e8b4297
Rainbow <33921411+sirisonte@users.noreply.github.com>
2019-10-10 21:08:48 +0200

    Update s3Upload.groovy

--- vars/s3Upload.groovy
+++ vars/s3Upload.groovy
@@ -1,1 +1,38 @@
-##
+def call(Map config) {
+
+    String artifactName = config.artifactName
+    String path = config.path
+    String version = config.version
+	  String assetRepoName = config.assetRepoName
+    
+    if (! artifactName || ! path || ! version || ! assetRepoName) {
+        error 'Missing parameter to pipeline!'
+    }
+    
+    String bucketname = 'testbucket-hbo'
+	String jenkinsScriptsDir = '/local/jenkins/jenkins-hbo.git/scripts'
+    
+    pipeline {
+        agent any
+	stages {
+		stage('Check if file exists in S3') {      
+			steps {
+			fileExists(artifactName, path)
+                
+            }
+          }
+        }
+    }
+}
+
+def fileExists(artifactName, path){
+    withAWS(credentials:'s3credentials') {
+	  def result = s3FindFiles bucket: bucketname, path: path, glob: artifactName
+	  if (result) {
+        println "File exists: " + result[0]                       
+    } else {
+        println "File does not exist fetching from github "
+							
+      }
+	}
+}
